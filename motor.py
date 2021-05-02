fe408aa4ff0fdf26a05118eebf63f251dcd0660e
Taric Folkes <taricdf@hotmail.com>
2021-05-02 14:17:41 -0400

    Indent Changes

--- motor.py
+++ motor.py
@@ -13,30 +13,30 @@ y=0
 
 try:
    while(1):
-       color = input("Enter the motor color: ")
-        if color == "orange":
-            out1 = 32
-            out2 = 31
-            out3 = 33
-            out4 = 29
+      color = input("Enter the motor color: ")
+      if color == "orange":
+          out1 = 32
+          out2 = 31
+          out3 = 33
+          out4 = 29
+        
+      elif color == "purple":
+          out1 = 13
+          out2 = 11
+          out3 = 15
+          out4 = 12
 
-        elif color == "purple":
-            out1 = 13
-            out2 = 11
-            out3 = 15
-            out4 = 12
+      elif color == "pink":
+          out1 = 18
+          out2 = 16
+          out3 = 22
+          out4 = 7
 
-        elif color == "pink":
-            out1 = 18
-            out2 = 16
-            out3 = 22
-            out4 = 7
-
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(out1,GPIO.OUT)
-        GPIO.setup(out2,GPIO.OUT)
-        GPIO.setup(out3,GPIO.OUT)
-        GPIO.setup(out4,GPIO.OUT)
+      GPIO.setmode(GPIO.BOARD)
+      GPIO.setup(out1,GPIO.OUT)
+      GPIO.setup(out2,GPIO.OUT)
+      GPIO.setup(out3,GPIO.OUT)
+      GPIO.setup(out4,GPIO.OUT)
 
       GPIO.output(out1,GPIO.LOW)
       GPIO.output(out2,GPIO.LOW)
