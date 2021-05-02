d5f9737ac44a48559a712b9b81d06223cdae6449
Taric Folkes <taricdf@hotmail.com>
2021-05-02 14:12:38 -0400

    Please work

--- motor.py
+++ motor.py
@@ -6,47 +6,45 @@ out2 = 0
 out3 = 0
 out4 = 0
 
-color = input("Enter the motor color you would you like to turn: ")
-if color == "orange":
-    out1 = 32
-    out2 = 31
-    out3 = 33
-    out4 = 29
-
-elif color == "purple":
-    out1 = 13
-    out2 = 11
-    out3 = 15
-    out4 = 12
-
-elif color == "pink":
-    out1 = 18
-    out2 = 16
-    out3 = 22
-    out4 = 7
-
 i=0
 positive=0
 negative=0
 y=0
 
-
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(out1,GPIO.OUT)
-GPIO.setup(out2,GPIO.OUT)
-GPIO.setup(out3,GPIO.OUT)
-GPIO.setup(out4,GPIO.OUT)
-
-print("First calibrate by giving some +ve and -ve values.....")
-
-
 try:
    while(1):
+       color = input("Enter the motor color: ")
+        if color == "orange":
+            out1 = 32
+            out2 = 31
+            out3 = 33
+            out4 = 29
+
+        elif color == "purple":
+            out1 = 13
+            out2 = 11
+            out3 = 15
+            out4 = 12
+
+        elif color == "pink":
+            out1 = 18
+            out2 = 16
+            out3 = 22
+            out4 = 7
+
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(out1,GPIO.OUT)
+        GPIO.setup(out2,GPIO.OUT)
+        GPIO.setup(out3,GPIO.OUT)
+        GPIO.setup(out4,GPIO.OUT)
+
       GPIO.output(out1,GPIO.LOW)
       GPIO.output(out2,GPIO.LOW)
       GPIO.output(out3,GPIO.LOW)
       GPIO.output(out4,GPIO.LOW)
-      x = int(input())
+
+
+      x = int(input("Enter Rotation Steps"))
       if x>0 and x<=400:
           for y in range(x,0,-1):
               if negative==1:
