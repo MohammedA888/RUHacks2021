8b1c488e8f1f3ccde0e47e9497d4809347cad532
Taric Folkes <taricdf@hotmail.com>
2021-05-02 15:33:29 -0400

    Gears Code

--- gear.py
+++ gear.py
@@ -6,6 +6,8 @@ in2 = 12
 en = 25
 temp1=1
 
+raw_input = 'r'
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(in1,GPIO.OUT)
 GPIO.setup(in2,GPIO.OUT)
@@ -20,7 +22,6 @@ print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
 print("\n")    
 
 while(1):
-
     x=raw_input()
     
     if x=='r':
